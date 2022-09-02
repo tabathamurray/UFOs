8931596915f22f6fd7c417ceacf26fe7d183cd92
tabathamurray <107326497+tabathamurray@users.noreply.github.com>
2022-09-01 22:57:31 -0500

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -77,16 +77,16 @@ function updateFilters() {
   function filterTable() {
   
     // 8. Set the filtered data to the tableData.
-    let filteredData = tableData;
+    let filterData = tableData;
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
     Object.entries(filters).forEach(([key, value]) => {
-      filteredData = filteredData.filter(row => row[key] === value);
+      filterData = filterData.filter(row => row[key] === value);
     });
   
     // 10. Finally, rebuild the table using the filtered data
-    buildTable(filteredData);
+    buildTable(filterData);
   }
   
   // 2. Attach an event to listen for changes to each filter
